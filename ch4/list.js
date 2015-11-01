12ec42392e5056472d146bc3a3df2c72440023c4
Armando Amador <arm.amador@gmail.com>
2015-11-01 08:47:04 -0500

    Create a prepend to list function and a nth function to find elements in a list

--- ch4/list.js
+++ ch4/list.js
@@ -19,9 +19,27 @@ function listToArray(list) {
   return array;
 }
 
+function prepend(value, list) {
+  return {value: value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
+
 array = [1, 2, 3];
 list = { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }
 
+
+
 console.log(arrayToList(array));
 console.log(listToArray(list));
+console.log(prepend(10, list));
+console.log(nth(list, 1));
+
 
