f9db587f5dd3c969e3e10fe067f3f0bb5b70e467
Armando Amador <arm.amador@gmail.com>
2015-11-01 08:31:50 -0500

    refactor list to array function

--- ch4/list.js
+++ ch4/list.js
@@ -11,30 +11,16 @@ function arrayToList(array) {
 
 function listToArray(list) {
   var array = [];
-  var temp = list;
-  var status = true;
 
-  while (status) {
-    array.push(temp.value)
-    if (!isEmptyObject(temp.rest)) {
-      temp = temp.rest;
-    } else {
-      status = false
-    }
+  for (var object = list; object; object = object.rest) {
+    array.push(object.value);
   }
+
   return array;
 }
 
-function isEmptyObject(obj) {
-    var name;
-    for (name in obj) {
-        return false;
-    }
-    return true;
-}
-
 array = [1, 2, 3];
-list = { value: 1, rest: { value: 2, rest: { value: 3, rest: {} } } }
+list = { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }
 
 console.log(arrayToList(array));
 console.log(listToArray(list));
